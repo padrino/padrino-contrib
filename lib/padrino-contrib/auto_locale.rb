9e7a1c368a6daf8b71cd34870b89bdd3b0032eff
Alex Coles <alex@alexcolesportfolio.com>
2010-10-02 21:35:03 +0800

    Make AutoLocale transparently prepend/strip locale
    * AutoLocale should alter request.path_info, so our controllers do not
      need to handle locale path parts in their mappings.
    * Conversely, the URL helpers should prepend the locale to the path.
    * Solution inspired by Sven Fuch's excellent Routing Filter for Rails:
      http://github.com/svenfuchs/routing-filter
      (and our matching RegEx adjusted, based on his work).
    
    Signed-off-by: Alex Coles <alex@alexcolesportfolio.com>

--- lib/padrino-contrib/auto_locale.rb
+++ lib/padrino-contrib/auto_locale.rb
@@ -24,14 +24,20 @@ module Padrino
         def switch_to_lang(lang)
           request.path_info.sub(/\/#{I18n.locale}/, "/#{lang}") if options.locales.include?(lang)
         end
+
+        def url(*args)
+          "/#{I18n.locale}#{super}"
+        end
       end # Helpers
 
       def self.registered(app)
         app.helpers Padrino::Contrib::AutoLocale::Helpers
         app.set :locales, [:en]
         app.before do
-          if request.path_info =~ /^\/(#{options.locales.join('|')})\/?/
+          if request.path_info =~ /^\/(#{options.locales.join('|')})(?=\/|$)/
             I18n.locale = $1.to_sym
+            request.path_info.sub!(/^\/(#{$1})\/?/) { $2 || '' }
+            request.path_info.replace('/') if request.path_info.empty?
           else
             I18n.locale = options.locales.first
           end
