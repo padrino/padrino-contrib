71396e15619413e2941bbffe9de6ec0e01434a4b
Davide D'Agostino <d.dagostino@lipsiasoft.com>
2011-07-28 16:11:39 +0200

    Bump to version 0.1.3

--- lib/padrino-contrib/auto_locale.rb
+++ lib/padrino-contrib/auto_locale.rb
@@ -28,15 +28,32 @@ module Padrino
 
       def self.registered(app)
         app.helpers Padrino::Contrib::AutoLocale::Helpers
+        app.extend ClassMethods
         app.set :locales, [:en]
         app.before do
-          if request.path_info =~ /^\/(#{options.locales.join('|')})(?=\/|$)/
+          if request.path_info =~ /^\/(#{options.locales.join('|')})\b/
             I18n.locale = $1.to_sym
           else
-            I18n.locale = options.locales.first
+            I18n.locale = options.locales[0]
           end
         end
-      end # self.registered
+
+        def self.padrino_route_added(route, verb, path, args, options, block)
+          route.instance_variable_set(:@original_path, "/:lang#{route.original_path}") unless route.original_path =~/:lang/
+        end
+      end
+
+      module ClassMethods
+        ##
+        # We need to add always a lang to all our routes
+        #
+        def url(*args)
+          params = args.extract_options!
+          params[:lang] = I18n.locale
+          args << params
+          super(*args)
+        end
+      end
     end # AutoLocale
   end # Contrib
 end # Padrino
