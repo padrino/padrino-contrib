33dadbf9b56d8ed9023783a8f7a51324f2921172
Davide D'Agostino <d.dagostino@lipsiasoft.com>
2011-09-26 18:01:07 +0200

    Bump to version 0.1.7

--- lib/padrino-contrib/flash_session.rb
+++ lib/padrino-contrib/flash_session.rb
@@ -11,27 +11,29 @@ module Padrino
     #
     # ==== Usage
     #
-    #  use Padrino::Contrib::FlashSession, settings.session_id
+    #  register Padrino::Contrib::FlashSession
     #
-    class FlashSession
+    module FlashSession
 
       def self.registered(app)
-        app.use Padrino::Contrib::FlashSession, app.session_id
+        app.use Padrino::Contrib::FlashSession::Middleware, app.session_id
       end
 
-      def initialize(app, session_key = 'session_id')
-        @app = app
-        @session_key = session_key.to_s
-      end
-
-      def call(env)
-        if env['HTTP_USER_AGENT'] =~ /^(Adobe|Shockwave) Flash/
-          params = ::Rack::Request.new(env).params
-          env['rack.session'] ||= {}
-          env['rack.session'][@session_key.to_sym] = params[@session_key] if params[@session_key].present?
+      module Middleware
+        def initialize(app, session_key = 'session_id')
+          @app = app
+          @session_key = session_key.to_s
         end
-        @app.call(env)
-      end
+
+        def call(env)
+          if env['HTTP_USER_AGENT'] =~ /^(Adobe|Shockwave) Flash/
+            params = ::Rack::Request.new(env).params
+            env['rack.session'] ||= {}
+            env['rack.session'][@session_key.to_sym] = params[@session_key] if params[@session_key].present?
+          end
+          @app.call(env)
+        end
+      end # Middleware
     end # FlashSession
   end # Contrib
 end # Padrino
