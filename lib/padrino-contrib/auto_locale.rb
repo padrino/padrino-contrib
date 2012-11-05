5855db18a131ccfc30a169a2a19481f7c8e53416
Michael Elfassy <michaelelfassy@napps.ca>
2012-11-04 20:44:20 -0500

    Make lang optional (use default lang if none defined in the url)

--- lib/padrino-contrib/auto_locale.rb
+++ lib/padrino-contrib/auto_locale.rb
@@ -22,7 +22,12 @@ module Padrino
         # This reload the page changing the I18n.locale
         #
         def switch_to_lang(lang)
-          request.path_info.sub(/\/#{I18n.locale}/, "/#{lang}") if settings.locales.include?(lang)
+          return unless settings.locales.include?(lang)
+          if request.path_info[/\/#{I18n.locale}\//]
+            request.path_info.sub(/\/#{I18n.locale}\//, "/#{lang}/") 
+          else
+            request.path_info.sub(/\/$/, "/#{lang}")
+          end
         end
       end # Helpers
 
@@ -40,7 +45,7 @@ module Padrino
         end
 
         def self.padrino_route_added(route, verb, path, args, options, block)
-          route.instance_variable_set(:@original_path, "/:lang#{route.original_path}") unless route.original_path =~/:lang/
+          route.instance_variable_set(:@original_path, "/(:lang)#{route.original_path}") unless route.original_path =~/:lang/
         end
       end
 
