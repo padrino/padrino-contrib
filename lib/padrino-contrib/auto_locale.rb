42dd7c6352c6dc040dbcb5774282c7cf2833f689
Michael Elfassy <michaelelfassy@napps.ca>
2012-11-06 09:30:10 -0500

    detect browser language and avoid catch all for root path

--- lib/padrino-contrib/auto_locale.rb
+++ lib/padrino-contrib/auto_locale.rb
@@ -36,16 +36,30 @@ module Padrino
         app.extend ClassMethods
         app.set :locales, [:en]
         app.before do
+          puts request.path_info.to_s
           if request.path_info =~ /^\/(#{settings.locales.join('|')})\b/
             I18n.locale = $1.to_sym
-          else
-            I18n.locale = settings.locales[0]
-            not_found if request.path_info !~ /^\/?$/
+          else 
+            # Guess the preferred language from the browser settings
+            for browser_locale in request.env['HTTP_ACCEPT_LANGUAGE'].split(",")
+              locale, usage = browser_locale.split(";")
+              if settings.locales.include?(locale.to_sym) #&& (usage.nil? || usage.split("=")[1].to_f > 0.5)
+                I18n.locale = locale.to_sym
+                break
+              end
+            end
+            # If none found use the default locale
+            I18n.locale ||= settings.locales[0]
           end
         end
 
         def self.padrino_route_added(route, verb, path, args, options, block)
           route.instance_variable_set(:@original_path, "/(:lang)#{route.original_path}") unless route.original_path =~/:lang/
+          if route.original_path == "/:lang/" || route.original_path == "/(:lang)/"
+            # add low priority so path doesn't become a catch all
+            args.first ||= {}
+            args.first.merge!(:priority=>:low) 
+          end
         end
       end
 
