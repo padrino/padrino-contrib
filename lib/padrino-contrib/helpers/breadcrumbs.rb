931143d32a050d01e21707d9cf5962f728049536
Darío Javier Cravero <dario@uxtemple.com>
2014-09-09 10:22:54 +0200

    Allowed breadcrumbs helper to be able to be registered loading its dependencies and added a working example app to it.
    @namusyaka I have a feeling we could rework the way the helper works;
    I've never used it but after looking at it because of @Signum's issue
    the fact that you can't really replace a breadcrumb but that you have to
    delete and add it again doesn't feel right. Thoughts?
    
    Fixes #28.

--- lib/padrino-contrib/helpers/breadcrumbs.rb
+++ lib/padrino-contrib/helpers/breadcrumbs.rb
@@ -1,12 +1,49 @@
 module Padrino
   module Contrib
     module Helpers
+      ##
+      # Sample usage
+      #
+      # # app/app.rb
+      # module Sample
+      #   class App < Padrino::Application
+      #     register Padrino::Contrib::Helpers::Breadcrumbs
+      #
+      #     helpers do
+      #       def breadcrumb
+      #         @breadcrumb ||= Padrino::Contrib::Helpers::Breadcrumb.new
+      #       end
+      #     end
+      #
+      #     before do
+      #       breadcrumb.set_home '/', 'Index'
+      #     end
+      #
+      #     get :index do
+      #       breadcrumbs breadcrumb
+      #     end
+      #
+      #     get :foo do
+      #       breadcrumb.add :foo, "/foo", "foo page"
+      #       breadcrumbs breadcrumb
+      #     end
+      #
+      #     get :test do
+      #       breadcrumb.add :test, "/test", "test page"
+      #       render :test
+      #     end
+      #   end
+      # end
+      #
+      # # app/views/test.slim
+      # = breadcrumbs breadcrumb
+      #
       class Breadcrumb
         attr_accessor :home, :items
-  
+
         DEFAULT_URL = "/"
         DEFAULT_CAPTION ="Home Page"
-  
+
         ##
         # Initialize breadcrumbs with default value.
         #
@@ -18,7 +55,7 @@ module Padrino
         def initialize
           reset!
         end
-  
+
         ##
         # Set the custom home (Parent) link.
         #
@@ -43,7 +80,7 @@ module Padrino
           }
           reset
         end
-  
+
         ##
         # Reset breadcrumbs to default or personal home.
         #
@@ -54,7 +91,7 @@ module Padrino
           self.items = []
           self.items << home
         end
-  
+
         ##
         # Reset breadcrumbs to default home.
         #
@@ -70,7 +107,7 @@ module Padrino
           }
           reset
         end
-  
+
         ##
         # Add a new breadcrumbs.
         #
@@ -101,7 +138,7 @@ module Padrino
           }
         end
         alias :<< :add
-  
+
         ##
         # Remove a breadcrumb.
         #
@@ -116,68 +153,88 @@ module Padrino
           items.delete_if { |item| item[:name] == name.to_sym }
         end
       end # Breadcrumb
-  
+
       module Breadcrumbs
-        ##
-        # Render breadcrumbs to view.
-        #
-        # @param [Breadcrumbs] breadcrumbs
-        #   The breadcrumbs to render into view.
-        #
-        # @param [Boolean] bootstrap
-        #  If true, render separation (useful with Twitter Bootstrap).
-        #
-        # @param [String] active
-        #  CSS class style set to active breadcrumb.
-        #
-        # @param [Hash] options
-        #   The HTML options to include in ul.
-        #
-        # @return [String] Unordered list with breadcrumbs
-        #
-        # @example
-        #  = breadcrumbs @breacrumbs
-        #  # Generates:
-        #  # <ul>
-        #  #   <li><a href="/foo">Foo Link</a></li>
-        #  #   <li class="active"><a href="/bar">Bar Link</a></li>
-        #  # </ul>
-        #
-        def breadcrumbs(breadcrumbs, bootstrap = false, active = "active", options = {})
-          content = ActiveSupport::SafeBuffer.new
-          breadcrumbs.items[0..-2].each do |item|
-            content << render_item(item, bootstrap)
+        class << self
+          ##
+          # Registers the Padrino::Contrib::Breadcrumbs helpers with the application.
+          #
+          # @param [Sinatra::Application] app The application that needs the breadcrumbs.
+          #
+          # @example
+          #   class Demo < Padrino::Application
+          #     register Padrino::Contrib::Helpers::Breadcrumbs
+          #   end
+          #
+          def registered(app)
+            require 'padrino-contrib/helpers/breadcrumbs'
+            app.helpers Padrino::Contrib::Helpers::Breadcrumbs::Helpers
           end
-          last = breadcrumbs.items.last
-          last_options = last[:options]
-          last = link_to(last[:caption], last[:url])
-  
-          classes = [options[:class], last_options[:class]].map { |class_name| class_name.to_s.split(/\s/) }
-          classes[0] << "breadcrumb"
-          classes[1] << active if active
-          options[:class], last_options[:class] = classes.map { |class_name| class_name * " " }
-  
-          content << content_tag(:li, last, last_options)
-          content_tag(:ul, content, options)
+          alias :included :registered
         end
-  
-        private
-        ##
-        # Private method to return list item.
-        #
-        # @param [Hash] item
-        #   The breadcrumb item.
-        #
-        # @param [Boolean] bootstrap
-        #   If true, render separation (useful with Twitter Bootstrap).
-        #
-        # @return [String] List item with breadcrumb
-        #
-        def render_item(item, bootstrap)
-          content = ActiveSupport::SafeBuffer.new
-          content << link_to(item[:caption], item[:url])
-          content << content_tag(:span, "/", :class => "divider") if bootstrap
-          content_tag(:li, content, item[:options])
+
+        module Helpers
+          ##
+          # Render breadcrumbs to view.
+          #
+          # @param [Breadcrumbs] breadcrumbs
+          #   The breadcrumbs to render into view.
+          #
+          # @param [Boolean] bootstrap
+          #  If true, render separation (useful with Twitter Bootstrap).
+          #
+          # @param [String] active
+          #  CSS class style set to active breadcrumb.
+          #
+          # @param [Hash] options
+          #   The HTML options to include in ul.
+          #
+          # @return [String] Unordered list with breadcrumbs
+          #
+          # @example
+          #  = breadcrumbs @breacrumbs
+          #  # Generates:
+          #  # <ul>
+          #  #   <li><a href="/foo">Foo Link</a></li>
+          #  #   <li class="active"><a href="/bar">Bar Link</a></li>
+          #  # </ul>
+          #
+          def breadcrumbs(breadcrumbs, bootstrap = false, active = "active", options = {})
+            content = ActiveSupport::SafeBuffer.new
+            breadcrumbs.items[0..-2].each do |item|
+              content << render_item(item, bootstrap)
+            end
+            last = breadcrumbs.items.last
+            last_options = last[:options]
+            last = link_to(last[:caption], last[:url])
+
+            classes = [options[:class], last_options[:class]].map { |class_name| class_name.to_s.split(/\s/) }
+            classes[0] << "breadcrumb"
+            classes[1] << active if active
+            options[:class], last_options[:class] = classes.map { |class_name| class_name * " " }
+
+            content << content_tag(:li, last, last_options)
+            content_tag(:ul, content, options)
+          end
+
+          private
+          ##
+          # Private method to return list item.
+          #
+          # @param [Hash] item
+          #   The breadcrumb item.
+          #
+          # @param [Boolean] bootstrap
+          #   If true, render separation (useful with Twitter Bootstrap).
+          #
+          # @return [String] List item with breadcrumb
+          #
+          def render_item(item, bootstrap)
+            content = ActiveSupport::SafeBuffer.new
+            content << link_to(item[:caption], item[:url])
+            content << content_tag(:span, "/", :class => "divider") if bootstrap
+            content_tag(:li, content, item[:options])
+          end
         end
       end # Breadcrumbs
     end # Helpers
